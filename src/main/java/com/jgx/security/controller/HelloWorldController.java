536df053de15ae837470c767916bc9742935f474
Jose Espinoza <espinozajgx@gmail.com>
2022-04-29 14:11:43 -0400

    code

---
+++ src/main/java/com/jgx/security/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.jgx.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("hello")
+    public String helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello "+name+"!!";
+    }
+
+    @RequestMapping("/")
+    public String healhCheck() {
+        return "I'm alive!!";
+    }
+}
---
+++ src/main/java/com/jgx/security/pojo/User.java
@@ -0,0 +1,23 @@
+package com.jgx.security.pojo;
+
+public class User {
+
+    private String username;
+    private String token;
+
+    public String getUser() {
+        return username;
+    }
+
+    public void setUser(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
